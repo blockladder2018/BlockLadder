c3d3cb1d89253e645879d85be7fc4e0fbf8a37d3
Alex Dai <xiaoched@amazon.com>
2018-05-15 21:22:53 -0700

    More stuff

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+
+  render() {
+    return (
+      <View style={{ flex: 1, paddingTop: 20 }}>
+        <Text>Block Ladder</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
